4331b9611cf479d6597794d1adb003c34b623929
tejashuske <tejashuske05@gmail.com>
2025-02-05 02:32:30 +0530

    backendd

---
+++ server/src/main/java/com/server/entity/Menu.java
@@ -0,0 +1,55 @@
+package com.server.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "menu")
+public class Menu {
+
+	@Id
+	@Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	@Column(name = "name")
+    private String name;
+	
+	@Column(name = "description")
+    private String description;
+	
+	@Column(name = "price")
+    private Double price;
+	
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+}
